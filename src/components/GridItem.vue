b2c414474af2eddba89ef4c57ab002a18ed58c3d
ggallici <gallicig94@gmail.com>
2020-03-13 14:31:38 -0300

    agrego wrappers y hago mismo test que con vue-grid-layout

---
+++ src/components/GridItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid-stack-item" data-gs-x=x data-gs-y=y data-gs-width=w data-gs-height=h>
+    <div class="grid-stack-item-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    x: {
+      type: Number,
+      required: true
+    },
+    y: {
+      type: Number,
+      required: true
+    },
+    w: {
+      type: Number,
+      required: true
+    },
+    h: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/components/GridLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid-stack backGround">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import 'gridstack/dist/gridstack.all'
+
+export default {
+  mounted() {
+    GridStack.init();
+  }
+};
+</script>
+
+<style scoped>
+@import "~gridstack/dist/gridstack.min.css";
+
+.backGround {
+  background: lightgoldenrodyellow;
+}
+</style>
