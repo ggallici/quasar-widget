5c8745784e81a4c1d4fbd26dede6c82a831b7c1d
ggallici <gallicig94@gmail.com>
2020-03-26 14:01:37 -0300

    elimino dragg de gridstack

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -13,21 +13,6 @@ export default {
       animate: true,
       resizable: {
         handles: 'se, sw'
-      },
-      acceptWidgets: true
-    });
-
-    $('.AAA').draggable({
-      helper: 'clone',
-      revert: 'invalid',
-      start: function( event, ui ) {
-        console.log("entro al evento start")
-      },
-      stop: function( event, ui ) {
-        console.log("entro al evento stop")
-        console.log("posision: " + JSON.stringify(ui.position))
-        console.log("offset: " + JSON.stringify(ui.offset))
-        //console.log("offset: " + JSON.stringify(ui.helper))
       }
     });
   }
